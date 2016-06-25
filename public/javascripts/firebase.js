e5f1241e40b92e58e3f9da78c75f368777d37a74
Dylan Harness <dharness@uwo.ca>
2016-06-25 14:31:46 -0400

    added basic login with firebase

---
+++ public/javascripts/firebase.js
@@ -0,0 +1,27 @@
+var config = {
+  apiKey: "AIzaSyBc37nzoXPRZIG8JiJeHW99d21oZLKUIGg",
+  authDomain: "bablot-faq.firebaseapp.com",
+  databaseURL: "https://bablot-faq.firebaseio.com",
+  storageBucket: "bablot-faq.appspot.com"
+};
+
+firebase.initializeApp(config);
+
+function showLogin() {
+  var provider = new firebase.auth.FacebookAuthProvider();
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
+
+    document.querySelector('.login-page').style.display = 'none';
+    document.querySelector('.faq-info').style.display = 'block';
+
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+});
+
+}
