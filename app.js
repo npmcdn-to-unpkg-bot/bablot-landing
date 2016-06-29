3eb7020cf3becc8e11a9ae45f61bd33753e18fac
Dylan Harness <dharness@uwo.ca>
2016-06-29 14:11:58 -0400

    ..

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.get('.well-known/acme-challenge/OkKRWTArdlkrbJvAIjEbq0ul4_Nk2QJKJ6Edmag', (req, res) => {
+
+app.get('/.well-known/acme-challenge/OkKRWTArdlkrbJvAIjEbq0ul4_Nk2QJKJ6Edmag', (req, res) => {
+  res.send('OkKRWTArdlkrbJvAIjEbq0ul4_Nk2QJKJ6Edmagj_o4.IP9Vd2bUb-AMw7Qn_4DEG9uIoKo_3ggtrZA1iJwDH7g');
+});
+
+app.get('/test', (req, res) => {
   res.send('OkKRWTArdlkrbJvAIjEbq0ul4_Nk2QJKJ6Edmagj_o4.IP9Vd2bUb-AMw7Qn_4DEG9uIoKo_3ggtrZA1iJwDH7g');
 });
 
