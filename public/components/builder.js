63d26b09a2deb9da48bacd38a115ac92d235409f
Dylan Harness <dharness@uwo.ca>
2016-06-29 22:56:42 -0400

    published

--- public/components/builder.js
+++ public/components/builder.js
@@ -1,10 +1,10 @@
 var builder = new Vue({
   el: '#builder',
   template: `
-    <div v-if="currentUser">
+    <div v-if="!currentUser">
       <login-page v-on:login-success="loginSuccess"></login-page>
     </div>
-    <div>
+    <div v-else>
       <h5> Questions | Answers </h5>
       <h5> {{ faqs.length }} / 15 </h5>
       <div v-for="faq in faqs">
@@ -34,16 +34,16 @@ var builder = new Vue({
   },
   methods: {
     loginSuccess: function(user) {
-      // this.currentUser = user;
-      // return firebase.database().ref(`users/${user.displayName}@${user.uid}`).once('value')
-      //   .then((showcase)=> {
-      //     if (showcase.val()) {
-      //       let faqs = showcase.val();
-      //       for (var key in faqs) {
-      //         this.faqs.push(faqs[key]);
-      //       }
-      //     }
-      //   });
+      this.currentUser = user;
+      return firebase.database().ref(`users/${user.displayName}@${user.uid}`).once('value')
+        .then((showcase)=> {
+          if (showcase.val()) {
+            let faqs = showcase.val();
+            for (var key in faqs) {
+              this.faqs.push(faqs[key]);
+            }
+          }
+        });
     },
     addRow: function () {
       let rand = Math.floor(Math.random() * 36);
