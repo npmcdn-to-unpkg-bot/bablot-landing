6440f75f8e45e95ed1fde0bae1706b3d0d068e57
Dylan Harness <dharness@uwo.ca>
2016-07-05 00:34:26 -0400

    changed routes so everything works

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bablot.co' });
 });
 
-/* GET Examples page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
 router.get('/examples', function(req, res, next) {
   res.render('examples');
 });
 
-/* GET FAQ page. */
-router.get('/faq', function(req, res, next) {
-  res.render('faq', { title: 'Bablot - FAQ' });
-});
-
-/* GET Builder page. */
 router.get('/builder', function(req, res, next) {
   res.render('builder');
-});
-
-/* GET Scheduler page. */
-router.get('/scheduler', function(req, res, next) {
-  res.render('scheduler');
 });
 
 module.exports = router;
