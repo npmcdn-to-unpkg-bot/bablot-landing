e6bd1d8a8c9f9bdb3795acae3f38528a2799a6ab
Dylan Harness <dharness@uwo.ca>
2016-07-05 01:01:10 -0400

    added favicon

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// uncomment after placing your favicon in /public/assets
+app.use(favicon(path.join(__dirname, 'public', 'assets', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
